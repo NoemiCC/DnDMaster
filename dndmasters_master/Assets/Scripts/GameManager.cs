5c616d926ac4d59076549d710ff069ae7734758e
Francisca Cattan <paqui@MacBook-Pro.local>
2020-10-04 19:51:49 -0300

    minigamed base

---
+++ dndmasters_master/Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource theMusic;
+    public bool startPlaying;
+    public BulletController theBS;
+    public static GameManager instance;
+
+    public int notesLeft = 10;
+    public bool gameOver = false;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameText;
+    public GameObject instructions;
+    void Start()
+    {
+        instance = this;
+        scoreText.text = "Quedan: " + notesLeft;
+        gameText.text = "Requiem";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!startPlaying)
+        {
+            if(!gameOver)
+            {
+                if(Input.anyKeyDown)
+                {
+                    instructions.SetActive(false);
+                    startPlaying = true;
+                    theBS.hasStarted = true;
+                    gameText.text = "";
+                    theMusic.Play();
+                }
+            }
+        
+        }
+        if(notesLeft <= 0)
+        {
+            startPlaying = false;
+            theMusic.Stop();
+            theBS.hasStarted = false;
+            gameText.text = "GANASTE";
+        }
+    }
+
+    public void NoteHit()
+    {
+        
+        Debug.Log("Hit!");
+        notesLeft -= 1;
+        scoreText.text = "Quedan: " + notesLeft;
+    }
+
+    public void NoteMissed()
+    {
+        Debug.Log("Miss!");
+        startPlaying = false;
+        theMusic.Stop();
+        theBS.hasStarted = false;
+        gameText.text = "PERDISTE";
+        gameOver = true;
+    }
+}
