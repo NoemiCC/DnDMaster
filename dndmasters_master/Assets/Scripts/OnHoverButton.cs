fbcfc5c008647b52380316fdbbc02097092b17e6
Francisca Cattan <paqui@MacBook-Pro.local>
2020-10-05 01:52:31 -0300

    UIfix

---
+++ dndmasters_master/Assets/Scripts/OnHoverButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class OnHoverButton : MonoBehaviour
+{
+	public GameObject arrowObject;
+
+	void Start()
+	{
+	    arrowObject.SetActive(false);
+	}
+
+    void OnMouseOver()
+    {
+	    arrowObject.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+	    arrowObject.SetActive(false);
+    }
+}
