1d6f9b164fe17959e1a3c00a933d7e126f5ef587
NoemiCC <nicrosby@uc.cl>
2020-10-04 23:51:40 -0300

    monje listo

---
+++ dndmasters_master/Assets/Scripts/MonkGame/MonkManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MonkManager : MonoBehaviour
+{
+    public GameObject gameCanvas;
+    public GameObject rulesCanvas;
+
+    public GameObject endCanvas;
+
+    bool startGame = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        gameCanvas.SetActive( false );
+        rulesCanvas.SetActive( true );
+        endCanvas.SetActive( false );
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!startGame && Input.anyKeyDown) {
+            startGame = true;
+            gameCanvas.SetActive( true );
+            rulesCanvas.SetActive( false );
+        }
+
+        if (endCanvas.activeSelf && Input.anyKeyDown) {
+            SceneManager.LoadScene("Campo_batalla");
+        }
+    }
+}
---
+++ dndmasters_master/Assets/Scripts/MonkGame/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject gameCanvas;
+
+    public GameObject endCanvas;
+    public Text endTitle;
+    public Text endPoints;
+
+    Rigidbody2D rb;
+    int points = 0;
+    public int maxPoints = 5;
+
+    Image timerBar;
+    public float maxTime = 5f;
+    float timeLeft;
+    bool gameOver = false;
+    
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        timerBar = GameObject.FindGameObjectWithTag("Timer").GetComponent<Image>();
+        timeLeft = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButton(0)) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            rb.position = mousePos;
+        }
+
+        if (timeLeft > 0 && !gameOver) {
+            timeLeft -= Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maxTime;
+        } else if (!gameOver) {
+            gameCanvas.SetActive( false );
+            endCanvas.SetActive( true );
+
+            endTitle.text = "Se acabo el tiempo";
+            endPoints.text += "0";
+            gameOver = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Point" && !gameOver) {
+            Destroy(collision.gameObject);
+            points++;
+
+            if (points >= maxPoints) {
+                gameCanvas.SetActive( false );
+                endCanvas.SetActive( true );
+
+                endTitle.text = "Lo lograste!";
+                endPoints.text += "100";
+                gameOver = true;
+            }
+
+        } else if (collision.gameObject.tag == "Bad" && !gameOver) {
+            gameCanvas.SetActive( false );
+            endCanvas.SetActive( true );
+
+            endTitle.text = "Has presionado un punto equivocado";
+            endPoints.text += "0";
+            gameOver = true;
+        }
+    }
+
+}
