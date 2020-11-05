41afa11348b14e0eb163520ba91d5593aa216bd9
Francisca Cattan <paqui@MacBook-Pro.local>
2020-11-05 17:12:32 -0300

    save scripts

---
+++ dndmasters_master/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    public int level = 1;
+    public int coins = 0;
+    public bool beginner = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadPlayer();
+    }
+
+	public void SavePlayer ()
+	{
+		SaveSystem.SavePlayer(this);
+	}
+
+	public void LoadPlayer ()
+	{
+		PlayerData data = SaveSystem.LoadPlayer();
+
+		level = data.level;
+    	coins = data.coins;
+    	beginner = data.beginner;
+	}
+
+}
